b5853df1a20264826a8d6a9d8661d9f9f701dbf8
Mahlatse Morapedi <mahlatse.morapedi@supplychainpartner.com>
2023-10-25 09:07:34 +0200

    Login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def dashboard
+  end
+
+  def login
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+  get 'sessions/dashboard'
+  get 'sessions/login'
+  get 'sessions/create'
+  get 'sessions/new'
+  get 'sessions/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'login', to: 'sessions#login'
+
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "sessions#dashboard"
 end
